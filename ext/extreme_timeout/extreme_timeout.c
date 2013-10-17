c189592b1b589830d29d03109013126e171df01c
Masaya Suzuki <masaya-suzuki@cookpad.com>
2013-10-17 17:12:31 +0900

    Call rb_bug instead of fprintf

--- ext/extreme_timeout/extreme_timeout.c
+++ ext/extreme_timeout/extreme_timeout.c
@@ -14,7 +14,7 @@ sleep_thread_main(void *_arg)
 {
     struct wait_args *arg = _arg;
     sleep(arg->timeout_sec);
-    fprintf(stderr, "Process exits(ExtremeTimeout::timeout)");
+    rb_bug("Process exits(ExtremeTimeout::timeout)");
     exit(arg->exitcode);
 }
 
