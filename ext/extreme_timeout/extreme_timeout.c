7d298e22923b8be3ab8f1d554532904cb6c2d80f
Eito Katagiri <eitoball@gmail.com>
2017-02-27 00:42:12 +0900

    add comment about alternative way to fix issue

--- ext/extreme_timeout/extreme_timeout.c
+++ ext/extreme_timeout/extreme_timeout.c
@@ -82,6 +82,10 @@ timeout_cb(VALUE block)
     return rb_funcall(block, rb_intern("call"), 0);
 }
 
+/*
+ * NOTE: Exposing this function name "timeout" caused segmentation
+ * fault in some environment (See PR #6).
+ */
 static VALUE
 timeout(int argc, VALUE *argv, VALUE self)
 {
